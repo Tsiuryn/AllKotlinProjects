076fb950005c6dee54bdd2b163112add22a2ff71
Tyurin_AV <Tsiuryn_AV@st.by>
2021-04-12 17:10:15 +0300

    mask

--- ForTest/src/main/java/com/ts/alex/fortest/MainActivity.kt
+++ ForTest/src/main/java/com/ts/alex/fortest/MainActivity.kt
@@ -1,31 +1,28 @@
 package com.ts.alex.fortest
 
+import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.ts.alex.fortest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.buttonShow.setOnClickListener{
 
-//        val editText = findViewById<EditText>(R.id.vFileName)
-//        editText.setCompoundDrawablesRelativeWithIntrinsicBounds(R.drawable.ic_get_app_24, 0, 0, 0)
+        }
 
-//        val inputLayout = findViewById<TextInputLayout>(R.id.vContainer)
-//        inputLayout.orientation = LinearLayout.HORIZONTAL
+        binding.vCheckbox.setTextColor(ColorStateList.valueOf(Color.GREEN))
 
-        val text = findViewById<EditText>(R.id.vText)
-//        val icon = text.background as GradientDrawable
-//        icon.setColor(Color.RED)
+        binding.vCheckbox.isEnabled = true
 
-        text.setBackgroundResource(R.drawable.input_layout)
-        val gd = text.background.current as GradientDrawable
-        gd.setStroke(2, Color.RED, 0f, 6f)
-//        gd.setColor(Color.parseColor("#000000"))
-//        gd.cornerRadii = floatArrayOf(30f, 30f, 30f, 30f, 0f, 0f, 30f, 30f)
 
 
 
--- workWithEditText/src/main/java/com/example/workwithedittext/mask/Mask.kt
+++ workWithEditText/src/main/java/com/example/workwithedittext/mask/Mask.kt
@@ -13,92 +13,156 @@ data class CharsMaskResult(
 
 val mask = "+375 (2\\9)999 99 99"
 
-fun checkEditTextWithMask(editText: TextInputEditText, mask: String) {
-    var isBackSpace = false
-    var ignore = false
-    var updatedMask = ""
-    for (item in startProgram(mask)) {
-        updatedMask += item.symbol
+class MaskEditText(private val editText: TextInputEditText, private val mask: String) {
+    private var textBefore = ""
+    private var textAfter = ""
+    private var cursorPosition = 0
+    init {
+        validateEditTextByMask()
     }
-    Log.d(
-        "TAG1",
-        "checkEditTextWithMask: ${startProgram(mask)}"
-    )
-    editText.setText(updatedMask)
-    editText.doBeforeTextChanged { text, start, before, count ->
-        isBackSpace = before > count
-        if (ignore) return@doBeforeTextChanged
-        ignore = true
-        Log.d(
-            "TAG1",
-            "checkEditTextWithMask: text $text start $start, before $before, count $count "
-        )
-        validationText(
-            isBackSpace = isBackSpace,
-            textBefore = text.toString(),
-            textAfter = editText.text.toString(),
+
+    private fun validateEditTextByMask() {
+        var isBackSpace = false
+        var ignore = false
+        var updatedMask = ""
+        val listMask = startProgram(mask)
+        for (item in listMask) {
+            updatedMask += item.symbol
+        }
+
+        editText.setText(updatedMask)
+        editText.doBeforeTextChanged {text, start, before, count ->
+            if (ignore) return@doBeforeTextChanged
+            textBefore = text.toString()
+        }
+        editText.doOnTextChanged { text, start, before, count ->
+            isBackSpace = before > count
+            if (ignore) return@doOnTextChanged
+            ignore = true
             cursorPosition = editText.selectionStart
-        )
+            var addedSymbol = ""
+            textAfter = editText.text.toString()
+            if(isBackSpace){
 
-        ignore = false
-    }
-}
+            }else{
+                addedSymbol = text!!.substring(cursorPosition-1, cursorPosition)
+                textBefore = "${text.substring(0, cursorPosition - 1)}${text.substring(cursorPosition-1, textAfter.length)}"
+            }
 
-private fun startProgram(mask: String): List<CharsMaskResult> {
-    val list = arrayListOf<CharsMaskResult>()
-    val maskArray = mask.toCharArray()
-    var position = 0
-    for (i in maskArray.indices) {
-        when (maskArray[i]) {
-            '9' -> {
-                if (i > 1) {
-                    if (maskArray[i - 1] == '\\' && maskArray[i - 2] == '\\') {
-                        list.add(CharsMaskResult(symbol = '9', rool = 'i', number = position++))
-                    } else {
-                        list.add(CharsMaskResult(symbol = '_', rool = '9', number = position++))
-                    }
-                }
-            }
-            'a' -> {
-                if (i > 1) {
-                    if (maskArray[i - 1] == '\\' && maskArray[i - 2] == '\\') {
-                        list.add(CharsMaskResult(symbol = 'a', rool = 'i', number = position++))
-                    } else {
-                        list.add(CharsMaskResult(symbol = '_', rool = 'a', number = position++))
-                    }
-                }
-            }
-            '*' -> {
-                if (i > 1) {
-                    if (maskArray[i - 1] == '\\' && maskArray[i - 2] == '\\') {
-                        list.add(CharsMaskResult(symbol = '*', rool = 'i', number = position++))
-                    } else {
-                        list.add(CharsMaskResult(symbol = '_', rool = '*', number = position++))
-                    }
-                }
-            }
-            '\\' -> continue
-            else -> {
-                list.add(CharsMaskResult(symbol = maskArray[i], rool = 'i', number = position++))
-            }
+            Log.d("TAG11", "validateEditTextByMask: $textBefore, $textAfter $addedSymbol")
+//        val result = validationText(
+//            isBackSpace = isBackSpace,
+//            listMask = listMask
+//        )
+//        editText.setText(result.text)
+//        editText.setSelection(result.cursorPosition)
+//            ignore = false
         }
     }
-    return list
-}
 
-private fun validationText(
-    isBackSpace: Boolean,
-    textBefore: String,
-    textAfter: String,
-    cursorPosition: Int
-): String {
-    if (isBackSpace) {
-
-    } else {
-        val arrayChar = textAfter.toCharArray()
-        val lastChar = arrayChar[arrayChar.size - 1]
-
+    private fun startProgram(mask: String): List<CharsMaskResult> {
+        val list = arrayListOf<CharsMaskResult>()
+        val maskArray = mask.toCharArray()
+        var position = 0
+        for (i in maskArray.indices) {
+            when (maskArray[i]) {
+                '9' -> {
+                    if (i > 1) {
+                        if (maskArray[i - 1] == '\\') {
+                            list.add(CharsMaskResult(symbol = '9', rool = 'i', number = position++))
+                        } else {
+                            list.add(CharsMaskResult(symbol = '_', rool = '9', number = position++))
+                        }
+                    }
+                }
+                'a' -> {
+                    if (i > 1) {
+                        if (maskArray[i - 1] == '\\' && maskArray[i - 2] == '\\') {
+                            list.add(CharsMaskResult(symbol = 'a', rool = 'i', number = position++))
+                        } else {
+                            list.add(CharsMaskResult(symbol = '_', rool = 'a', number = position++))
+                        }
+                    }
+                }
+                '*' -> {
+                    if (i > 1) {
+                        if (maskArray[i - 1] == '\\' && maskArray[i - 2] == '\\') {
+                            list.add(CharsMaskResult(symbol = '*', rool = 'i', number = position++))
+                        } else {
+                            list.add(CharsMaskResult(symbol = '_', rool = '*', number = position++))
+                        }
+                    }
+                }
+                '\\' -> continue
+                else -> {
+                    list.add(CharsMaskResult(symbol = maskArray[i], rool = 'i', number = position++))
+                }
+            }
+        }
+        return list
     }
-    return ""
+
+    private fun validationText(
+        isBackSpace: Boolean,
+        listMask: List<CharsMaskResult>
+    ): Result {
+        if (isBackSpace) {
+            return setBackSpace(
+                listMask = listMask
+            )
+
+        } else {
+            return addChar(
+                listMask = listMask
+            )
+            val arrayChar = textAfter.toCharArray()
+
+        }
+        return Result("", 0)
+    }
+
+    fun addChar(listMask: List<CharsMaskResult>): Result {
+        return  if(cursorPosition >= listMask.size){
+            Result(
+                text = textBefore,
+                cursorPosition = cursorPosition
+            )
+        } else {
+            val addedChar = textAfter.substring(cursorPosition -1 , cursorPosition)
+            Log.d("TAG1", "addChar: $addedChar")
+            return Result(textAfter, cursorPosition)
+        }
+    }
+
+    private fun setBackSpace(
+        listMask: List<CharsMaskResult>
+    ): Result {
+        val deletedChar = deletedSymbol(textBefore = textBefore, cursorPosition = cursorPosition)
+
+        val roolChar = listMask[cursorPosition - 1].rool
+        Log.d("TAG1", "setBackSpace: $cursorPosition $textBefore $deletedChar $roolChar")
+
+        return when (roolChar){
+            'i' -> Result(text = textBefore, cursorPosition = cursorPosition )
+            'a' -> Result(text = "$textBefore", cursorPosition = cursorPosition - 1 )
+            '*' -> Result(text = "$textBefore", cursorPosition = cursorPosition - 1 )
+            '9' -> Result(text = "$textBefore", cursorPosition = cursorPosition - 1 )
+            else -> Result(text = "", cursorPosition = 0)
+        }
+    }
+
+    private fun deletedSymbol (textBefore: String, cursorPosition: Int): String{
+        return if(cursorPosition > 1 ){
+            textBefore.substring(cursorPosition-1, textBefore.length)
+        } else {
+            textBefore
+        }
+    }
+
 }
+
+data class Result(
+    val text: String,
+    val cursorPosition: Int
+)
 
